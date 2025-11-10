6d8049fb6ca398207fefb3c064e1192e12df31f9
sussy_3k <congthcstp@gmail.com>
2025-11-10 08:14:00 +0700

    giulia: fix Phone Definitions
    Change-Id: Idd322f6580f93d4a3ceb82d0814e16c6d905270e

--- infinity_giulia.mk
+++ infinity_giulia.mk
@@ -38,10 +38,3 @@ TARGET_HAS_UDFPS := true
 
 # Google Apps
 WITH_GAPPS := true
-
-PRODUCT_SYSTEM_PROPERTIES += \
-	ro.product.marketname=OnePlus\ Ace\ 5 \
-	ro.infinity.soc=SM8650 \
-	ro.infinity.battery=6450\ mAh \
-	ro.infinity.display=LTPO\ 1264\ x\ 2780,\ 120\ Hz \
-	ro.infinity.camera=50MP\ +\ 8MP\ +\ 2MP
