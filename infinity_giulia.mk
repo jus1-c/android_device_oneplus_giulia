784d93bb0ea46c635633ada4732b0f23ef55b605
sussy_3k <congthcstp@gmail.com>
2025-11-11 04:48:05 +0700

    giulia: fix infinity mk file (2)
    Change-Id: I4475303922cd658ab1af85b9105d9064dc7594a6

--- infinity_giulia.mk
+++ infinity_giulia.mk
@@ -1,3 +1,35 @@
+#
+# Copyright (C) 2021-2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from giulia device
+$(call inherit-product, device/oneplus/giulia/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
+
+PRODUCT_NAME := lineage_giulia
+PRODUCT_DEVICE := giulia
+PRODUCT_MANUFACTURER := OnePlus
+PRODUCT_BRAND := OnePlus
+PRODUCT_MODEL := CPH2691
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="CPH2691IN-user 15 UKQ1.231108.001 V.R4T3.220ef38-2f6786-311918 release-keys" \
+    BuildFingerprint=OnePlus/CPH2691IN/OP5D3BL1:15/UKQ1.231108.001/V.R4T3.220ef38-2f6786-311918:user/release-keys \
+    DeviceName=OP5D3BL1 \
+    DeviceProduct=CPH2691 \
+    SystemDevice=OP5D3BL1 \
+    SystemName=CPH2691
+
 # Maintainer Name
 INFINITY_MAINTAINER := "Sussy3k"
 
